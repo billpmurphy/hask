c5c7050512a684eba6a5d84837ab7dcd9b803bf5
Bill Murphy <billpmurphy92@gmail.com>
2015-07-31 17:57:30 -0400

    fix #8 and clean up setup.py

--- setup.py
+++ setup.py
@@ -9,25 +9,26 @@ except ImportError:
 
 des = "Haskell language features and standard library ported to Python"
 setup(
-        name = 'hask',
-        version = hask.__version__,
-        description=des,
-        long_description = open('README.md').read(),
-        author='Bill Murphy',
-        author_email='billpmurphy92@gmail.com',
-        url='https://github.com/billpmurphy/hask',
-        packages=['hask'],
-        package_data={'': ['LICENSE', 'README.md']},
-        include_package_data=True,
-        install_requires=[],
-        license=open('LICENSE').read(),
-        zip_safe=False,
-        classifiers=(
-            'Development Status :: 2 - Pre-Alpha',
-            'Intended Audience :: Developers',
-            'Natural Language :: English',
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7'
-            ),
-    )
+    name='hask',
+    version=hask.__version__,
+    description=des,
+    long_description=open('README.md').read(),
+    author='Bill Murphy',
+    author_email='billpmurphy92@gmail.com',
+    url='https://github.com/billpmurphy/hask',
+    packages=['hask', 'hask.lang', 'hask.Python', 'hask.Data',
+              'hask.Control'],
+    package_data={'': ['LICENSE', 'README.md']},
+    include_package_data=True,
+    install_requires=[],
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+        ),
+)
